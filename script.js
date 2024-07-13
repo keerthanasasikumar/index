b5d046a6cb200f1fa86b7c913c357d79c3fc3f6b
keerthanasasikumar <166113227+keerthanasasikumar@users.noreply.github.com>
2024-07-13 20:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("signup");
+var y = document.getElementById("login");
+var z = document.getElementById("btn");
+function signup() {
+    x.style.left = "0px";
+    y.style.left = "350px";
+    z.style.left = "0px";
+}
+function login() {
+    x.style.left = "-400px";
+    y.style.left = "5px";
+    z.style.left = "50px";  
+}
